42001cc2f83712861959cd3957b185bee0afedad
werep002 <pablo.weremczuk@gmail.com>
2014-08-05 18:21:18 -0300

    Moduralizo las páginas, agrego la opcion de contactar por mail.

--- www/js/app.js
+++ www/js/app.js
@@ -12,7 +12,7 @@
         $scope.showDetail = function(index) {
             var selectedItem = $data.items[index];
             $data.selectedItem = selectedItem;
-            $scope.ons.navigator.pushPage('detail.html', {
+            $scope.ons.navigator.pushPage('templates/detail.html', {
                 title: selectedItem.title
             });
         }
@@ -52,8 +52,8 @@
             globalVariable.scope.debugText = "error " + status;
         }
 
-        $scope.httpSuccess = function(html) {
-            globalVariable.scope.debugText = "Exito!: " + html;
+        $scope.httpSuccess = function(data, status, headers, config) {
+            globalVariable.scope.debugText = "Exito!: " + headers;
         }
     });
 
@@ -162,9 +162,36 @@
     });
 
 
+
     module.controller('ContactController', function($scope) {
-        $scope.callSucursal = function() {
-            //window.open('tel:45239431', '_new');
+        $scope.cods_sucursal = {
+            VICENTE_LOPEZ: 0,
+            CAPITAL: 1
+        };
+
+        $scope.callSucursal = function(cod_sucursal) {
+            console.log(cod_sucursal);
+            switch (cod_sucursal) {
+                case this.cods_sucursal.VICENTE_LOPEZ:
+                    window.open('tel:01147979435', '_new');
+                    break;
+                case this.cods_sucursal.CAPITAL:
+                    window.open('tel:45462853', '_new');
+                    break;
+            }
+        }
+        $scope.emailSucursal = function(cod_sucursal) {
+            console.log(cod_sucursal);
+            switch (cod_sucursal) {
+                case this.cods_sucursal.VICENTE_LOPEZ:
+                    var link = "mailto:info@nakaoutdoors.com.ar" + "?subject=" + escape("Consulta") + "&body=" + escape("Estimados Naka Outdoors:\n");
+                    window.open(link, '_new');
+                    break;
+                case this.cods_sucursal.CAPITAL:
+                    var link = "mailto:urquiza@nakaoutdoors.com.ar" + "?subject=" + escape("Consulta") + "&body=" + escape("Estimados Naka Outdoors:\n");
+                    window.open(link, '_new');
+                    break;
+            }
         }
     });
 
