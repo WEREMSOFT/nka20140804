4496f2004ab0fe5debfdd58897fe2f7cbcda3a0c
werep002 <pablo.weremczuk@gmail.com>
2014-08-08 17:49:56 -0300

    agrego un plugin para lockear la pantalla

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,23 @@
 (function() {
     'use strict';
-    var module = angular.module('myApp', ['onsen.directives', 'ContactForm', 'List', 'Login', 'GridMenu', 'Alert']);
+    var app = angular.module('myApp', ['onsen.directives', 'ContactForm', 'List', 'Login', 'GridMenu', 'Alert']);
+
+    app.initialize = function() {
+        this.bindEvents();
+    }
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    app.bindEvents = function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    }
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    app.onDeviceReady = function() {
+        // set to either landscape
+        screen.lockOrientation('portrait');
+    }
 })();
