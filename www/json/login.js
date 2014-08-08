b033009cd3bfb822a895dc04d9d8aed1b12e7e99
werep002 <pablo.weremczuk@gmail.com>
2014-08-08 20:07:32 -0300

    otro intento de restrinjir la rotación

---
+++ www/json/login.js
@@ -0,0 +1,42 @@
+[{
+    loggedIn: true,
+
+    nombre: Pablo,  
+    apellido: Weremczuk, 
+    mail: pablo.weremczuk@gmail.com
+    foto: 
+    nakarios
+    compras
+    carrito
+    telefono
+    tel-cel
+    direccion
+    provincia
+    provincia de residencia
+    terminalDeEnvio
+    Terminal de envio
+    localidad
+    localidad
+    partido
+    partido
+    codigoPostal
+    codigo postal
+    datosFacturacion
+    Datos de facturación,
+    pueden ser Consumidor Final,
+    responsable inscripto,
+    etc.
+    Pedidos
+
+
+    pedidosFinalizados
+    Un array de objetos,
+    formato de los mismos a determinar
+    pedidosPendientes
+    Idem anterior
+    pedidosCancelados
+    Idem anterior
+
+
+
+}]
