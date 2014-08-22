ebae81fda5a5dfb8fc469f2a96bac3397accf0ab
werep002 <pablo.weremczuk@gmail.com>
2014-08-22 00:07:54 -0300

    implemento login completo

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,27 @@
 (function() {
     'use strict';
-    var app = angular.module('myApp', ['onsen.directives', 'ngSanitize', 'Product','Category','ContactForm', 'List', 'Login', 'GridMenu' ]);
+    var app = angular.module('myApp', ['onsen.directives', 'ngSanitize', 'Product', 'Category', 'ContactForm', 'List', 'Login', 'GridMenu']);
 
-    app.factory('userData',function(){
-    	return {legedIn: false, profileData:null}
+    app.factory('userData', function() {
+    	var returnValue = {logedIn: false, profileData: null, userName: null, password: null};
+    	returnValue.reset = function()
+    	{
+
+    		this.logedIn = false;
+    		this.profileData = null;
+    		window.localStorage.setItem('logedIn', false);
+    		window.localStorage.setItem('profileData', null);
+    	}
+
+    	if(window.localStorage.getItem('logedIn'))
+    	{
+    		returnValue.logedIn = window.localStorage.getItem('logedIn');
+    		returnValue.profileData = JSON.parse(window.localStorage.getItem('profileData'));
+    		returnValue.userName = window.localStorage.getItem('userName');
+    		returnValue.password = window.localStorage.getItem('password');
+    	}
+
+        return returnValue;
     });
 
 })();
--- www/js/controllers/LoginController.js
+++ www/js/controllers/LoginController.js
@@ -10,6 +10,9 @@
                 alert('La dirección de mail no es válida');
                 return;
             }
+            window.localStorage.setItem("user", $scope.user);
+            window.localStorage.setItem("password", $scope.password);
+            window.localStorage.setItem("logedIn", true);
             var request = $http({
                 method: "post",
                 url: 'http://www.nakaoutdoors.com.ar/usuarios/applogin.json',
@@ -37,8 +40,11 @@
         $scope.httpSuccess = function(data, status, headers, config) {
             if (data.result.logedIn === 1) {
                 $scope.userData.profileData = data.result.Usuario;
-                $scope.userData.logedIn =  true;
-                ons.navigator.pushPage('templates/FormProfile.html', {'userData':$scope.userData});
+                window.localStorage.setItem("profileData", JSON.stringify(data.result.Usuario));
+                $scope.userData.logedIn = true;
+               
+                ons.navigator.resetToPage('templates/PageHome.html');
+
             } else if (data.result.logedIn === -2) {
                 alert('Nombre de usuario o contraseña invalidas');
             }
@@ -71,22 +77,44 @@
             $scope.debugText = "Exito!: " + data;
             $scope.userName = data.split('"Ver datos de ')[1].split('"')[0];
             alert("Bienvenido " + $scope.userName + "!!!!!");
-            ons.navigator.pushPage('templates/FormProfile.html');
+            ons.navigator.resetToPage('templates/FormProfile.html');
         }
 
         $scope.init = function() {
             console.log('inicializando......' + ons.navigator.getCurrentPage().name);
-            if (ons.navigator.getCurrentPage().name === 'templates/FormProfile.html')
-            {
+            if (ons.navigator.getCurrentPage().name === 'templates/FormProfile.html') {
                 //$scope.userData = ons.navigator.getCurrentPage().options.userData; 
                 return;
-            } 
-            if ($scope.userData.logedIn === true) {
-                ons.navigator.pushPage('templates/FormProfile.html');
-            } else {
-                $scope.user = 'naka5@hotmail.com';
-                $scope.password = 'naka5'
             }
+            if ($scope.userData.logedIn === true) {
+                ons.navigator.resetToPage('templates/FormProfile.html');
+            } else {
+                $scope.user = window.localStorage.getItem('user');
+                $scope.password = window.localStorage.getItem('password');
+            }
+        }
+
+        $scope.logout = function() {
+            var request = $http({
+                method: "get",
+                url: 'http://www.nakaoutdoors.com.ar/usuarios/logout',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+            // Store the data-dump of the FORM scope.
+            request.success(this.logoutSuccess);
+            // Store the data-dump of the FORM scope.
+            request.error(this.logoutError);
+        }
+
+        $scope.logoutError = function(data, status, headers, config) {
+            console.log("Oops! Algo ha salido mal. Reintenta en un momento");
+        }
+
+        $scope.logoutSuccess = function(data, status, headers, config) {
+            $scope.userData.reset();
+            ons.navigator.resetToPage('templates/PageHome.html');
         }
 
     });
