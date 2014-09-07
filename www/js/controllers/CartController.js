c94f3eaad597a6dc56ea5f3c4316b564527b3663
werep002 <pablo.weremczuk@gmail.com>
2014-09-07 15:30:39 -0300

    Agrego la posibilidad de eliminar del carrito.

--- www/js/controllers/CartController.js
+++ www/js/controllers/CartController.js
@@ -70,33 +70,10 @@
         }
 
         $scope.init = function() {
-            console.log('cargando carrito de compras');
-            $scope.working = true;
-            var request = $http({
-                method: "get",
-                url: 'http://www.nakaoutdoors.com.ar/webservices/carrito.json',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            });
-
-            // Store the data-dump of the FORM scope.
-            request.success(this.httpRecorverCartSuccess);
-
-
-            // Store the data-dump of the FORM scope.
-            request.error(this.httpRecorverCartError);
+           $scope.shoppingCart.refreshCartDetails();
         }
 
-        $scope.httpRecorverCartError = function(data, status, headers, config) {
-            alert("Oops! Algo ha salido mal. Reintenta en un momento");
-        }
-
-        $scope.httpRecorverCartSuccess = function(data, status, headers, config) {
-            console.log(data);
-            $scope.products = data.result.items;
-            $scope.working = false;
-        }
+       
 
         $scope.enviarPedido = function() {
             $scope.working = true;
@@ -107,9 +84,10 @@
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
 
-                data: '_method=PUT&data[Pedido][nombre]=' + $scope.nombre + '&data[Pedido][apellido]=' + $scope.apellido + '&data[Pedido][mail]=' + $scope.mail + '&data[Pedido][cod_area]=' + $scope.codArea + '&data[Pedido][celular]=' + $scope.celular + '&data[Pedido][tipo_seguro]=' + $scope.tipoSeguro + '&data[Pedido][forma_envio]=' + $scope.formaEnvio + '&data[Pedido][direccion]=' + $scope.direccion + '&data[Pedido][terminal]=' + $scope.terminal + '&data[Pedido][codigo_postal]=' + $scope.codigoPostal + '&data[Pedido][provincia_id]=' + $scope.provincia + '&data[Pedido][forma_pago]=' + $scope.formaDePago + '&data[Pedido][telefono]=' +  $scope.telefono + '&data[Pedido][observaciones]=' + $scope.observaciones + '&data[Pedido][iva_facturacion]=' + $scope.ivaFacturacion + '&data[Pedido][razon_social]=' + $scope.razonSocial + '&data[Pedido][cuit]' + $scope.cuit + '&'
+                data: '_method=PUT&data[Pedido][nombre]=' + $scope.nombre + '&data[Pedido][apellido]=' + $scope.apellido + '&data[Pedido][mail]=' + $scope.mail + '&data[Pedido][cod_area]=' + $scope.codArea + '&data[Pedido][celular]=' + $scope.celular + '&data[Pedido][tipo_seguro]=' + $scope.tipoSeguro + '&data[Pedido][forma_envio]=' + $scope.formaEnvio + '&data[Pedido][direccion]=' + $scope.direccion + '&data[Pedido][terminal]=' + $scope.terminal + '&data[Pedido][codigo_postal]=' + $scope.codigoPostal + '&data[Pedido][provincia_id]=' + $scope.provincia + '&data[Pedido][forma_pago]=' + $scope.formaDePago + '&data[Pedido][telefono]=' + $scope.telefono + '&data[Pedido][observaciones]=' + $scope.observaciones + '&data[Pedido][iva_facturacion]=' + $scope.ivaFacturacion + '&data[Pedido][razon_social]=' + $scope.razonSocial + '&data[Pedido][cuit]' + $scope.cuit + '&'
 
             });
+
 
 
             // Store the data-dump of the FORM scope.
@@ -130,5 +108,36 @@
             ons.navigator.popPage();
             $scope.working = false;
         }
+
+        $scope.eliminarDelCarrito = function(id) {
+            console.log('eliminando producto del carrito de compras');
+            $scope.working = true;
+            var request = $http({
+                method: "get",
+                url: 'http://www.nakaoutdoors.com.ar/articulos/carrito_del.json?id=' + id,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+
+            // Store the data-dump of the FORM scope.
+            request.success(this.httpEliminarDelCarritoSuccess);
+
+
+            // Store the data-dump of the FORM scope.
+            request.error(this.httpEliminarDelCarritoError);
+        }
+
+        $scope.httpEliminarDelCarritoError = function(data, status, headers, config) {
+            alert("Oops! Algo ha salido mal. Reintenta en un momento");
+            $scope.working = false;
+        }
+
+        $scope.httpEliminarDelCarritoSuccess = function(data, status, headers, config) {
+            console.log("Item eliminado con exito");
+            $scope.shoppingCart.refreshCartDetails();
+            $scope.working = false;
+        }
+
     });
 })();
