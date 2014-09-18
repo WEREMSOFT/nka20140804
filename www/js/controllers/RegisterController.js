f19ca9a6e457e3fabfc7c6df8e38120c393166cf
werep002 <pablo.weremczuk@gmail.com>
2014-09-18 14:14:04 -0300

    Agrego el webservice de registración

--- www/js/controllers/RegisterController.js
+++ www/js/controllers/RegisterController.js
@@ -14,14 +14,13 @@
             $scope.isWorking = true;
             var request = $http({
                 method: "post",
-                url: 'http://www.nakaoutdoors.com.ar/usuarios/registro',
+                url: 'http://www.nakaoutdoors.com.ar/usuarios/registro.json',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
 
-                data: '_method=POST&data[Usuario][nombre]=' + $scope.nombre + '&data[Usuario][apellido]=' + $scope.apellido + '&data[Usuario][mail]=' + $scope.email + '&data[Usuario][provincia_id]=' + $scope.provincia + '&data[Usuario][login]=' + $scope.userName + '&data[Usuario][password]=' + $scope.password + '&data[Usuario][check_password]=' + $scope.passwordVerification + '&data[Usuario][tipo_usuario_id]=2&data[Usuario][habilitado]=0&'
+                data: '_method=POST&data[Usuario][nombre]=' + $scope.nombre + '&data[Usuario][apellido]=' + $scope.apellido + '&data[Usuario][liam]=' + $scope.email + '&data[Usuario][provincia_id]=' + $scope.provincia + '&data[Usuario][login]=' + $scope.userName + '&data[Usuario][password]=' + $scope.password + '&data[Usuario][check_password]=' + $scope.password + '&data[Usuario][ant]=' + $scope.hashAnt + '&data[Usuario][mail]=' + $scope.hashMail + '&data[Usuario][maps]=&'
             });
-
 
             // Store the data-dump of the FORM scope.
             request.success(this.httpSuccess);
@@ -33,14 +32,58 @@
         }
 
         $scope.httpError = function(data, status, headers, config) {
-            alert("Oops! Algo ha salido mal. Reintenta en un momento");
+            promptError("Oops! Algo ha salido mal. Reintenta en un momento");
         }
 
         $scope.httpSuccess = function(data, status, headers, config) {
             console.log(data);
-            $scope.products = data.result;
+            $scope.result = data.result;
+            if ($scope.result.code === 0) {
+                prompt($scope.result.messagge);
+            } else {
+                promptError($scope.result.message);
+            }
+            $scope.isWorking = false;
+            $scope.goBack();
+        }
+        $scope.getHash = function() {
+            if ($scope.isWorking === true) return;
+            $scope.isWorking = true;
+            var request = $http({
+                method: "get",
+                url: 'http://www.nakaoutdoors.com.ar/webservices/hash.json',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+
+
+            // Store the data-dump of the FORM scope.
+            request.success(this.httpGetHashSuccess);
+
+
+            // Store the data-dump of the FORM scope.
+            request.error(this.httpError);
+        }
+
+        $scope.goBack = function() {
+            if ($scope.isWorking === true) return;
+            ons.navigator.popPage();
+        }
+
+        $scope.httpGetHashSuccess = function(data) {
+            var hashObj = data.result;
+            $scope.hashMail = hashObj.mail;
+            $scope.hashAnt = hashObj.ant;
+
+            $scope.register();
+        }
+
+        $scope.httpError = function(data, status, headers, config) {
+            promptError("Ooosp!, algo ha salido mal, reintente en un momento");
             $scope.isWorking = false;
         }
+
     });
 
     module.directive('moduleComboProvincias', function() {
