2aa2d5198435391258079c31cda3bf1520fc1213
werep002 <pablo.weremczuk@gmail.com>
2014-10-08 19:35:15 -0300

    agrego payload

--- www/js/app.js
+++ www/js/app.js
@@ -293,7 +293,7 @@ function onNotificationGCM(e) {
         case 'message':
             // this is the actual push notification. its format depends on the data model from the push server
             alert('message = ' + e.message + ' msgcnt = ' + e.msgcnt);
-            alert('productId = ' + e.productID + ' CategoryID=' + e.categoryID);
+            alert('productId = ' + e.payload.productID + ' CategoryID=' + e.payload.categoryID);
             alert(e);
             break;
 
