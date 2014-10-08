9f4a6e7a9f4d4d3c34d9ce2f777f058487acf0d9
werep002 <pablo.weremczuk@gmail.com>
2014-10-08 19:19:44 -0300

    mando datos adicionales

--- www/js/app.js
+++ www/js/app.js
@@ -292,7 +292,7 @@ function onNotificationGCM(e) {
         case 'message':
             // this is the actual push notification. its format depends on the data model from the push server
             alert('message = ' + e.message + ' msgcnt = ' + e.msgcnt);
-            alert('productId = ' + e.productID + ' CategoryID=' e.categoryID);
+            alert('productId = ' + e.productID + ' CategoryID=' + e.categoryID);
             alert(e);
             break;
 
