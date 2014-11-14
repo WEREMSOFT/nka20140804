7cada41238cfe6e93c0d38304d72165267a6e31c
werep002 <pablo.weremczuk@gmail.com>
2014-11-14 18:41:07 -0300

    hago que el evento del backButton sea ejecutado en la fase de captura.

--- www/js/app.js
+++ www/js/app.js
@@ -387,4 +387,4 @@ function googleAnalyticsTrakEventError() {
 document.addEventListener("backbutton", function(e) {
     alert("THE BACK BUTTON WAS HIT!");
     e.preventDefault();
-}, false);
+}, true);
