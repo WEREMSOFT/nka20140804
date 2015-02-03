b1d2a4d398ebd80a07127dd6b74530edb8393838
werep002 <pablo.weremczuk@gmail.com>
2015-02-03 18:07:22 -0300

    modifico el controller para sustituir los select

--- www/js/controllers/CartController.js
+++ www/js/controllers/CartController.js
@@ -25,15 +25,11 @@
         $scope.products = [];
         $scope.isCart = true;
 
-
-
-
-        $scope.formaEnvio = '';
-
+        $scope.formaEnvio = {};
 
         $scope.observaciones = "";
 
-
+        $scope.tipoDeEnvio = {};
         $scope.buyOptions = {};
 
 
@@ -45,7 +41,7 @@
 
         $scope.addToCart = function() {
             if ($scope.product.options && !$scope.talle.id) {
-                prompt('Debe elegir una opción');
+                messageWindow('Debe elegir una opción');
                 return;
             }
             $scope.isWorking = true;
@@ -59,19 +55,15 @@
                 data: '_method=POST&data[Articulo][id]=' + $scope.product.id + '&data[Articulo][cantidad]=' + $scope.cantidad + '&data[Articulo][opcion]=' + $scope.talle.id + '&data[Articulo][nombre_opcion]=' + $scope.talle.name + '&'
             });
 
-
-
             // Store the data-dump of the FORM scope.
             request.success(this.httpSuccess);
 
-
             // Store the data-dump of the FORM scope.
             request.error(this.httpError);
-
         }
 
         $scope.httpError = function(data, status, headers, config) {
-            promptError('Oops! Algo ha salido mal. Reintenta en un momento', null, 'Sin Conección', 'Bueno');
+            messageWindowError('Oops! Algo ha salido mal. Reintenta en un momento', null, 'Sin Conección', 'Bueno');
         }
 
         $scope.httpSuccess = function(data, status, headers, config) {
@@ -79,6 +71,13 @@
             $scope.products = data.result;
             $scope.isWorking = false;
             var strConfirmationText = ' ' + $scope.cantidad + ' item' + ($scope.cantidad > 1 ? 's' : '') + ' agregado' + ($scope.cantidad > 1 ? 's' : '') + ' al carrito.';
+            /*            navigator.notification.confirm(
+                            strConfirmationText, // message
+                            $scope.onPromtAddToCartOk, // callback to invoke with index of button pressed
+                            'Éxito', // title
+                            ['Ver Carrito', 'Seguir'] // buttonLabels
+                        );*/
+
             ons.notification.confirm({
                 buttonLabel: 'Sí',
                 title: 'Información',
@@ -97,6 +96,8 @@
             });
         }
 
+
+
         $scope.onPromtAddToCartOk = function(buttonIndex) {
             switch (buttonIndex) {
                 case 1:
@@ -113,7 +114,7 @@
 
         $scope.completarDatosEnvio = function() {
             if (!$scope.shoppingCart.cartData.items.length) {
-                prompt("Su carrito de compras esta vacío");
+                messageWindow("Su carrito de compras esta vacío");
                 return;
             }
             ons.navigator.pushPage('templates/forms/CartDatosEnvio.html')
@@ -122,7 +123,7 @@
 
         $scope.enviarPedido = function() {
             if ($scope.formPedido.$invalid) {
-                prompt("Debe completar todos los campos marcados en rojo");
+                messageWindow("Debe completar todos los campos marcados en rojo");
                 return;
             }
             $scope.isWorking = true;
@@ -133,7 +134,7 @@
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
 
-                data: '_method=PUT&data[Pedido][nombre]=' + userData.profileData.nombre + '&data[Pedido][apellido]=' + userData.profileData.apellido + '&data[Pedido][mail]=' + userData.profileData.mail + '&data[Pedido][cod_area]=' + userData.profileData.cod_area + '&data[Pedido][celular]=' + userData.profileData.celular + '&data[Pedido][tipo_seguro]=' + $scope.tipoSeguro + '&data[Pedido][forma_envio]=' + $scope.formaEnvio + '&data[Pedido][direccion]=' + userData.profileData.direccion + '&data[Pedido][terminal]=' + userData.profileData.terminal + '&data[Pedido][codigo_postal]=' + userData.profileData.codigo_postal + '&data[Pedido][provincia_id]=' + userData.profileData.provincia_id + '&data[Pedido][forma_pago]=' + $scope.formaDePago + '&data[Pedido][telefono]=' + userData.profileData.telefono + '&data[Pedido][observaciones]=' + $scope.observaciones + '&data[Pedido][iva_facturacion]=' + userData.profileData.iva_facturacion + '&data[Pedido][razon_social]=' + userData.profileData.razon_social + '&data[Pedido][cuit]=' + userData.profileData.cuit + '&'
+                data: '_method=PUT&data[Pedido][nombre]=' + userData.profileData.nombre + '&data[Pedido][apellido]=' + userData.profileData.apellido + '&data[Pedido][mail]=' + userData.profileData.mail + '&data[Pedido][cod_area]=' + userData.profileData.cod_area + '&data[Pedido][celular]=' + userData.profileData.celular + '&data[Pedido][tipo_seguro]=' + $scope.tipoSeguro + '&data[Pedido][forma_envio]=' + $scope.formaEnvio.id + '&data[Pedido][direccion]=' + userData.profileData.direccion + '&data[Pedido][terminal]=' + userData.profileData.terminal + '&data[Pedido][codigo_postal]=' + userData.profileData.codigo_postal + '&data[Pedido][provincia_id]=' + userData.profileData.provincia_id + '&data[Pedido][forma_pago]=' + $scope.formaDePago + '&data[Pedido][telefono]=' + userData.profileData.telefono + '&data[Pedido][observaciones]=' + $scope.observaciones + '&data[Pedido][iva_facturacion]=' + userData.profileData.iva_facturacion + '&data[Pedido][razon_social]=' + userData.profileData.razon_social + '&data[Pedido][cuit]=' + userData.profileData.cuit + '&'
             });
 
 
@@ -148,11 +149,11 @@
         }
 
         $scope.httpEnviarPedidoError = function(data, status, headers, config) {
-            promptError('Oops! Algo ha salido mal. Reintenta en un momento', null, 'Sin Conección', 'Bueno');
+            messageWindowError('Oops! Algo ha salido mal. Reintenta en un momento', null, 'Sin Conección', 'Bueno');
         }
 
         $scope.httpEnviarPedidoSuccess = function(data, status, headers, config) {
-            prompt('Su pedido ha sido enviado con éxito.', goBackOnePage);
+            messageWindow('Su pedido ha sido enviado con éxito.', goBackOnePage);
             $scope.shoppingCart.refreshCartDetails();
             $scope.isWorking = false;
         }
@@ -176,12 +177,12 @@
         }
 
         $scope.httpEliminarDelCarritoError = function(data, status, headers, config) {
-            promptError('Oops! Algo ha salido mal. Reintenta en un momento', null, 'Sin Conección');
+            messageWindowError('Oops! Algo ha salido mal. Reintenta en un momento', null, 'Sin Conección');
             $scope.isWorking = false;
         }
 
         $scope.httpEliminarDelCarritoSuccess = function(data, status, headers, config) {
-            prompt('Item eliminado.', null, 'Éxito');
+            messageWindow('Item eliminado.', null, 'Éxito');
             $scope.shoppingCart.refreshCartDetails();
             $scope.isWorking = false;
         }
